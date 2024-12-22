350d33008482270f4f1777b2cf186bf4f4f5943d
Dnayneshwar1Ghate <dnyaneshwarghate1010@gmail.com>
2024-12-22 16:15:26 +0530

    new

---
+++ src/test/java/Locaters.java
@@ -0,0 +1,45 @@
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.joda.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Locaters {
+	public static void main(String[] args) throws InterruptedException {
+
+			// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.gecko.driver", "F:\\Software\\geckodriver-v0.32.2-win32\\geckodriver.exe");
+		 
+		WebDriver driver = new FirefoxDriver();
+			//implicit wait - 2 seconds time out
+		//System.setProperty("webdriver.chrome.driver", "/Users/rahulshetty/Documents/chromedriver");
+		//WebDriver driver = new ChromeDriver();
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+		driver.findElement(By.name("inputPassword")).sendKeys("hello123");
+		driver.findElement(By.className("signInBtn")).click();
+		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		Thread.sleep(1000);//
+		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("John");
+		driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("john@rsa.com");
+		driver.findElement(By.xpath("//input[@type='text'][2]")).clear();
+		driver.findElement(By.cssSelector("input[type='text']:nth-child(3)")).sendKeys("john@gmail.com");
+		driver.findElement(By.xpath("//form/input[3]")).sendKeys("9864353253");
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		System.out.println(driver.findElement(By.cssSelector("form p")).getText());
+		driver.findElement(By.xpath("//div[@class='forgot-pwd-btn-conainer']/button[1]")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector("#inputUsername")).sendKeys("rahul");
+		driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.id("chkboxOne")).click();
+		driver.findElement(By.xpath("//button[contains(@class,'submit')]")).click();
+	}
+}
