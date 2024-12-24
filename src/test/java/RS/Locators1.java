0d7107a0255faab0d4a7e95781a5082e674669e2
Dnayneshwar1Ghate <dnyaneshwarghate1010@gmail.com>
2024-12-24 07:29:03 +0530

    new 2 class added

---
+++ src/test/java/RS/Locators1.java
@@ -0,0 +1,22 @@
+package RS;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Locators1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		
+		driver.manage().window().maximize();
+		
+		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/followingsibling::button[1]")).getText());
+		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/parent::div/button[2]")).getText());
+	}
+}
---
+++ src/test/java/RS/WindowActivities.java
@@ -0,0 +1,19 @@
+package RS;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WindowActivities {
+	public static void main(String[] args) {
+// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://google.com");
+		driver.navigate().to("https://rahulshettyacademy.com");
+		driver.navigate().back();
+		driver.navigate().forward();
+	}
+}
